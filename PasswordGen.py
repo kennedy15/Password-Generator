e88684fb0b3240c41093985171880f6664675236
kennedy15 <33329262+kennedy15@users.noreply.github.com>
2018-08-02 22:26:29 -0400

    Create PasswordGen.py

---
+++ PasswordGen.py
@@ -0,0 +1,28 @@
+import random
+import math
+import string
+
+class Password_Generator():
+
+    def __init__(object):
+
+        word_dict = open('words_alpha.txt', 'r').readlines()
+
+        first_half = str(random.choice(word_dict))
+
+        if len(first_half) < 4:
+            pass
+
+        else:
+            first_half = first_half[0:4]
+
+        char = string.ascii_letters + string.digits + string.punctuation
+
+        size = int(raw_input("How long would you like your password? "))
+        size = size - 5
+
+        second_half =  ''.join(random.choice(char) for _ in range(size))
+
+        print 'Your password is: %s-%s' % (first_half, second_half)
+
+Password_Generator()
