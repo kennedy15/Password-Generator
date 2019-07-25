6e98f1139884ba27084918eac4c5324f5c6044eb
kennedy15 <33329262+kennedy15@users.noreply.github.com>
2019-07-25 10:28:28 -0400

    Create PasswordGeneratorC#.cs

---
+++ PasswordGeneratorC#.cs
@@ -0,0 +1,29 @@
+       
+       Password Generator written in C#
+       
+       public string Generate_Password()
+        {
+            int length = Word_Lines.Length;
+
+            Random random = new Random();
+
+            StringBuilder second_half = new StringBuilder();
+
+            string first_half = Word_Lines[random.Next(length)];
+
+            int size = 5;
+
+            if (first_half.Length > 6)
+            {
+                int end = first_half.Length;
+                first_half = first_half.Remove(4, end - 4);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                second_half.Append(characters[random.Next(characters.Length)]);
+            }
+
+            return first_half + second_half.ToString();
+
+        }
